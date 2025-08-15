8ecf31742c6179ff96c78ba0754af7c71cb7ae97
nutblaster07 <abhishek.roy.21@aot.edu.in>
2025-08-16 03:13:04 +0530

    variables

---
+++ variablses.js
@@ -0,0 +1,7 @@
+const accountId=122;
+const accountName="John Doe";
+const accountBalance=1000.50;
+console.log("Account ID:", accountId);
+console.log("Account Name:", accountName);  
+console.log("Account Balance:", accountBalance);
+console.log("hii");
