58d3e928a9e4e7749235977b6ff86121ac2644d9
nutblaster07 <abhishek.roy.21@aot.edu.in>
2025-08-16 04:08:06 +0530

    push

--- variablses.js
+++ variablses.js
@@ -1,7 +1,10 @@
+"use strict";//treat all JS code as ES6 or newer version
 const accountId=122;
 const accountName="John Doe";
 const accountBalance=1000.50;
 console.log("Account ID:", accountId);
 console.log("Account Name:", accountName);  
 console.log("Account Balance:", accountBalance);
-console.log("hii");
+
+
+  
